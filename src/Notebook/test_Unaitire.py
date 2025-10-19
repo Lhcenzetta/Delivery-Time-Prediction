21b155794ef461bb0c4ef0b7a596436ed1e903ff
lait-zet <lhcenztta18@gmail.com>
2025-10-19 23:54:01 +0100

    some update in test

--- src/Notebook/test_Unaitire.py
+++ src/Notebook/test_Unaitire.py
@@ -22,21 +22,3 @@ def test_split_dimensions(sample_data):
     trainX, testX, trainY, testY = split_data(df)
     assert len(trainX) == len(trainY)
     assert len(testX) == len(testY)
-
-
-X = pd.DataFrame({
-    'feature1': np.random.rand(100),
-    'feature2': np.random.rand(100)
-})
-y = X['feature1'] * 10 + X['feature2'] * 5 + np.random.randn(100)
-
-train_X, test_X, train_Y, test_Y = train_test_split(X, y, test_size=0.2, random_state=42)
-def test_mae_threshold():
-    results = GridSearch_CV(train_X, test_X, train_Y, test_Y)
-    max = 10.0
-
-    mae_RF = results["RandomForestRegressor"]["MAE"]
-    mae_SVR = results["SVR"]["MAE"]
-
-    assert mae_RF < max
-    assert mae_SVR < max
